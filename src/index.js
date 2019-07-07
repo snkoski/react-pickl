23a06355776321f6a479612c06df1342b412fef4
snkoski <shawnnkoski@gmail.com>
2019-07-07 18:59:47 -0400

    Change BrowserRouter to HashRouter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './store';
-import { BrowserRouter as Router} from 'react-router-dom'; 
+import { HashRouter as Router} from 'react-router-dom'; 
 
 import './index.css';
 import * as serviceWorker from './serviceWorker';
