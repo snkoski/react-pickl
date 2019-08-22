0f484108d7cdc3305cdda33731ef18aa8896a2bf
snkoski <shawnnkoski@gmail.com>
2019-08-22 11:09:10 -0400

    Separate navbar into logged-in and not loggeed-in versions instead of using ternarys for each button.

--- src/components/Nav/NavItems.js
+++ src/components/Nav/NavItems.js
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import { Menu } from 'antd';
 import classes from './AntNav.module.css';
 
-
 const NavItems = (props) => {
     let [selected, setSelected] = useState('5')
 
@@ -11,7 +10,7 @@ const NavItems = (props) => {
         console.log("TARGET", e)
         setSelected(e.key)
     }
-    return (
+    if (props.isAuthenticated) {return (
         <Menu
             theme="dark"
             mode="horizontal"
@@ -20,16 +19,27 @@ const NavItems = (props) => {
             onClick={handleSelect}
         >
             <Menu.Item className={classes.Pickl} key="5"><Link to='/'>Pickl</Link></Menu.Item>
-            { props.isAuthenticated 
-                ? <Menu.Item className={classes.Right} key="3"><Link to='/logout'>Logout</Link></Menu.Item>
-                : <Menu.Item className={classes.Right} key="2"><Link to='/register'>Register</Link></Menu.Item> 
-            }
-            { props.isAuthenticated 
-                ? (selected !== '4') ? <Menu.Item className={classes.Right} key="4"><Link to='/user'>User Profile</Link></Menu.Item> : <Menu.Item className={classes.Right} key="6"><Link to='/'>Games</Link></Menu.Item>
-                : <Menu.Item className={classes.Right} key="1"><Link to='/login'>Login</Link></Menu.Item>
-            }
+            <Menu.Item className={classes.Right} key="3"><Link to='/logout'>Logout</Link></Menu.Item>
+                
+                <Menu.Item className={classes.Right} key="4"><Link to='/user'>User Profile</Link></Menu.Item>
+                <Menu.Item className={classes.Right} key="6"><Link to='/'>Games</Link></Menu.Item>
         </Menu>
-    )
+    )} else {
+        return (
+            <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={[selected]}
+            style={{ lineHeight: '64px' }}
+            onClick={handleSelect}
+        >
+            <Menu.Item className={classes.Pickl} key="5"><Link to='/'>Pickl</Link></Menu.Item>
+                 <Menu.Item className={classes.Right} key="2"><Link to='/register'>Register</Link></Menu.Item> 
+                 <Menu.Item className={classes.Right} key="1"><Link to='/login'>Login</Link></Menu.Item>
+        </Menu>
+        )
+    }
 }
 export default NavItems;
+
 
